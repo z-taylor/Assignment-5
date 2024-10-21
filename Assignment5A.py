105510632b5e1df663d5e3af916c5c655945f102
Zachary Taylor <120347442+z-taylor@users.noreply.github.com>
2024-10-20 23:48:29 -0400

    save

--- Assignment5A.py
+++ Assignment5A.py
@@ -9,7 +9,6 @@
 
 import pygame, sys, random, json
 from pygame.locals import *
-
 #setup pygame window
 pygame.init()
 clock = pygame.time.Clock()
@@ -17,14 +16,12 @@ width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Lab 8C Zachary Taylor')
 moveSpeed = 5
-
 #make the surface and the rect
 surf = pygame.Surface((800, 800))
 surf.fill((0, 0, 0))
 smallRect = pygame.Rect(380, 380, 40, 40)
 pygame.draw.rect(surf, (0, 255, 255), smallRect)
 screen.blit(surf, (0, 0))
-
 #make random asteroid function
 def makeAsteroid():
     side = random.randint(1, 4)
@@ -42,7 +39,6 @@ def makeAsteroid():
     x, y = ( (width-sX) if x>(width-sX) else x ), ( (height-sY) if y>(height-sY) else y )
     #print(f"side:{side}, sX:{sX}, sY:{sY}, x:{x}, y:{y}, dX:{dX}, dY:{dY}\n")
     return sX, sY, x, y, dX, dY
-
 def reset(score):
     ticker = 0
     ticker2 = 0
@@ -57,6 +53,7 @@ def reset(score):
     screen.blit(gameOverText, (0, 0))
     pygame.display.flip()
     pygame.time.delay(3000)
+    return collide, ticker, ticker2, asteroids, spawnRate, timer, counter
 #set all variables used in the loop
 ticker = 0
 ticker2 = 0
@@ -89,14 +86,14 @@ while run:
             new_rect = smallRect.move(dirX, dirY)
             if 0 <= new_rect.x <= width-smallRect.width and 0 <= new_rect.y <= height-smallRect.height:
                 smallRect = new_rect
-    if ticker == 0: #make an asteroid every so often and change the spawn rate by -0.25 seconds every 5 spawns until it reaches 1 second
+    if ticker == 0: #make an asteroid every so often and change the spawn rate by -0.5 seconds every 5 spawns until it reaches 1 second
         sX, sY, x, y, dX, dY = makeAsteroid()
         asteroidTemp = pygame.Rect(x, y, sX, sY)
         asteroids[asteroidID] = (asteroidTemp, dX, dY)
         asteroidID += 1
         counter += 1 if counter<5 else 0
         if counter == 5:
-            spawnRate -= 15 if spawnRate > 60 else 0
+            spawnRate -= 30 if spawnRate > 60 else 0
             counter = 0
     if ticker2 == 60:
         timer += 1
@@ -121,7 +118,7 @@ while run:
         pygame.draw.rect(surf, (0, 255, 255), smallRect)
         screen.blit(surf, (0, 0))
     if collide:
-        reset(timer)
+        collide, ticker, ticker2, asteroids, spawnRate, timer, counter = reset(timer)
     pygame.display.flip()
     clock.tick(60)
     ticker +=1 if ticker<spawnRate else -spawnRate
